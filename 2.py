b0c279ee550a1ea53da418b97489932674ac68b6
Dhanuthi <126179110+Dhanuthi@users.noreply.github.com>
2023-09-14 14:22:52 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+score = int(input("Enter your scaore: "))
+
+if score>90 and score<100:
+    print("your grade is A")
+elif score>80 and score<89:
+    print("your grade is B")
+elif score>70 and score<79:
+    print("your grade is C")
+elif score>60 and score<69:
+    print("your grade is D")
+elif score<60:
+        print("your grade is F")
+
+     
